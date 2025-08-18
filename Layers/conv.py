a060d8b37c7b597e72c427f7ae5575c91c975d91
Archit Bansal <archit.ban123@gmail.com>
2025-08-19 03:46:57 +0530

    Added batching and nb for simple CNN for binary classification

--- Layers/conv.py
+++ Layers/conv.py
@@ -11,52 +11,55 @@ class Conv2DLayer(Layer):
         self.previous_hidden_statechannels_out = channels_out
         
         self.weights = torch.normal(0,sqrt(2/(channels_in*window_size[0]*window_size[1])),(channels_out,channels_in,window_size[0],window_size[1]))
-        self.bias = torch.zeros((channels_out,1))
+        self.bias = torch.zeros((channels_out))
         
         self.previous_hidden_state = None
         self.w_grads = torch.zeros(self.weights.shape)
         self.b_grads = torch.zeros(self.bias.shape)
 
     def output_shape(self, input_shape):
-        x1 = input_shape[1]-(self.window_size[0]-1)
+        x1 = input_shape[-2]-(self.window_size[0]-1)
         x1 = ceil(x1/self.stride[0])
-        x2 = input_shape[2]-(self.window_size[1]-1)
+        x2 = input_shape[-1]-(self.window_size[1]-1)
         x2 = ceil(x2/self.stride[1])
         return (x1,x2)
 
     def __call__(self, X):
+        X = torch.unsqueeze(X,1)
         output_shape = self.output_shape(X.shape)
         self.previous_hidden_state = X
-        H = torch.zeros((self.channels_out,output_shape[0],output_shape[1]))
+        H = torch.zeros((X.shape[0],self.channels_out,output_shape[0],output_shape[1]))
 
-        for ch_out in range(H.shape[0]):
-            for i in range(H.shape[1]):
-                for j in range(H.shape[2]):
-                    row_idx, col_idx = i*self.stride[0], j*self.stride[1]
-                    mat_in = X[:,row_idx:row_idx+self.window_size[0],col_idx:col_idx+self.window_size[1]]
-                    H[ch_out][i][j] = torch.sum(torch.mul(mat_in,self.weights[ch_out,:,:,:])) + self.bias[ch_out]
+        for i in range(H.shape[-2]):
+            for j in range(H.shape[-1]):
+                row_idx, col_idx = i*self.stride[0], j*self.stride[1]
+                mat_in = X[:,:,:,row_idx:row_idx+self.window_size[0],col_idx:col_idx+self.window_size[1]]
+                H[:,:,i,j] = torch.sum(torch.mul(mat_in,self.weights), dim=(-3,-2,-1)) + self.bias
+
         return H
 
     def grad_calc(self, grads):
-        #calculating wgrads
-        for n_out in range(grads.shape[0]):
-            for i in range(grads.shape[1]):
-                for j in range(grads.shape[2]):
-                    row_idx, col_idx = i*self.stride[0], j*self.stride[1]
-                    mat_in = self.previous_hidden_state[:,row_idx:row_idx+self.window_size[0],col_idx:col_idx+self.window_size[1]]
-                    self.w_grads[n_out] += mat_in*grads[n_out][i][j]
-                    self.b_grads[n_out] += grads[n_out][i][j]
+        self.w_grads = torch.zeros(self.weights.shape)
+        self.b_grads = torch.zeros(self.bias.shape)
 
-        self.h_grads = torch.zeros(self.previous_hidden_state.shape)
-        for n_out in range(grads.shape[0]):
-            for i in range(grads.shape[1]):
-                for j in range(grads.shape[2]):
-                    row_idx, col_idx = i*self.stride[0], j*self.stride[1]
-                    self.h_grads[:,row_idx:row_idx+self.window_size[0],col_idx:col_idx+self.window_size[1]] += grads[n_out][i][j]*self.weights[n_out]
-                    mat_in = self.previous_hidden_state[:,row_idx:row_idx+self.window_size[0],col_idx:col_idx+self.window_size[1]]
+        #calculating wgrads
+        for i in range(grads.shape[-2]):
+            for j in range(grads.shape[-1]):
+                row_idx, col_idx = i*self.stride[0], j*self.stride[1]
+                mat_in = self.previous_hidden_state[:,:,:,row_idx:row_idx+self.window_size[0],col_idx:col_idx+self.window_size[1]]
+                grads_in = grads[:,:,i,j]
+                self.w_grads += torch.sum(torch.mul(grads_in[:,:,None,None,None],mat_in), dim=0)
+                self.b_grads += torch.sum(grads[:,:,i,j], dim=0)
+
+        self.h_grads = torch.zeros(self.previous_hidden_state.shape).squeeze(1)
+        for i in range(grads.shape[-2]):
+            for j in range(grads.shape[-1]):
+                row_idx, col_idx = i*self.stride[0], j*self.stride[1]
+                grads_in = grads[:,:,i,j]
+                self.h_grads[:,:,row_idx:row_idx+self.window_size[0],col_idx:col_idx+self.window_size[1]] += torch.sum(grads_in[:,:,None,None,None]*torch.unsqueeze(self.weights,0), dim=1)
         
         return self.h_grads
 
     def step(self, lr_rate):
         self.weights -= lr_rate*self.w_grads
-        self.bias -= lr_rate*self.b_grads 
+        self.bias -= lr_rate*self.b_grads
--- Layers/flatten.py
+++ Layers/flatten.py
@@ -7,10 +7,10 @@ class Flatten(Layer):
 
     def __call__(self, X):
         self.orig_shape = X.shape
-        return X.view(-1,1)
+        return X.view(self.orig_shape[0],-1).T
 
     def grad_calc(self, grads):
-        return grads.view(self.orig_shape)
+        return grads.T.view(self.orig_shape)
 
     def step(self, *args):
         pass
